b2242bf095b716c93479fd2ed93adca2d7a97176
Hafiz-Pangteh <hafiz.pangteh@outlook.com>
2023-07-25 10:32:43 +0700

    hide database config

--- helpers/db.php
+++ helpers/db.php
@@ -1,8 +1,8 @@
 <?php
-$hostname = "127.0.0.1";
-$username = "root";
-$password = "Farn_24498";
-$database = "php_colorfull09af";
+$hostname = ""; //host
+$username = ""; //database username
+$password = "";  //database password
+$database = ""; //database name
 $port = 3306;
 // mysqli_report(MYSQLI_REPORT_OFF);
 $connection = mysqli_connect($hostname, $username, $password, $database, $port);
